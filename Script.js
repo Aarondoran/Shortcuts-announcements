6fec4ec990afccc417192053d1e8d6310628c319
Aaron doran <aarondoran@outlook.ie>
2024-03-28 01:20:22 +0000

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,18 @@
+ // Define variables for each shortcut
+        var shortcuts = {
+            shortcutid-1234: "Text for shortcut 1",
+            shortcutid-: "Text for shortcut 2",
+            shortcutid-: "Text for shortcut 3",
+            shortcutid-: "Text for shortcut 4",
+            shortcutid-: "Text for shortcut 5",
+            shortcutid-: "Text for shortcut 6",
+            shortcutid-: "Text for shortcut 7",
+            shortcutid-: "Text for shortcut 8",
+            shortcutid-: "Text for shortcut 9",
+            shortcutid-: "Text for shortcut 10"
+        };
+
+        // Function to return the text when called
+        function getText() {
+            return myText;
+        }
