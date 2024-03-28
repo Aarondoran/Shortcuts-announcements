646ae2bd17e2bee8e8c3a9a4b7019da2084bc48a
Aaron doran <aarondoran@outlook.ie>
2024-03-28 01:29:17 +0000

    Update and rename Script.js to index.html

--- Script.js
+++
@@ -1,7 +0,0 @@
- // Define variables for each shortcut
-        var shortcuts = ('hello')
-
-        // Function to return the text when called
-        function getText() {
-            return myText;
-        }
